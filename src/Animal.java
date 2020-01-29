aeb9043aececb1f49f2a01a36ee9921be4052502
Cassidy Carpenter <caca8883@colorado.edu>
2020-01-28 22:08:33 -0700

    added some code to animal and main

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,43 @@
 public class Animal {
+
+    private String food;
+    private int legs;
+    private String location;
+
+    public Animal(){}
+
+    public Animal(String foodItEats, int numLegs, String livesHere){
+        this.food = foodItEats;
+        this.legs = numLegs;
+        this.location = livesHere;
+    }
+
+    public String getFood(){
+        return food;
+    }
+
+    public void setFood(String newFood){
+        this.food = newFood;
+    }
+
+    public int getLegs(){
+        return legs;
+    }
+
+    public void setLegs(int newLegs){
+        this.legs = newLegs;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public void setLocation(String newLoc){
+        this.location = newLoc;
+    }
+
+
+
+
+
 }
